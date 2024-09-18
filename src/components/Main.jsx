bb9eb59046726d5cbf9fd141e3f7c11efc293bcc
Vitor figueiredo <vitor.figueiredo@neogov.com.br>
2024-09-18 16:45:56 -0300

    Button transition

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ export const Main = () => {
   return (
     <section className='text-white h-3/4 py-24 mt-2'>
       <p className='font-paysage font-black text-7xl w-[60%] cursor-default mb-24'>Hi, I am Vitor Figueiredo, a web developer</p>
-      <span className='bg-[#F2F0F0]/15 px-12 py-3 text-2xl rounded-xl cursor-pointer hover:bg-white hover:text-black'>get me a tour ➞</span>
+      <span className='bg-[#F2F0F0]/15 px-12 py-3 text-2xl rounded-xl cursor-pointer hover:bg-white hover:text-black ease-in-out duration-300 '>get me a tour ➞</span>
     </section>
   )
 
