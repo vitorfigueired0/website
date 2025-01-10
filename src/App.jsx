54362fc3a30031c795713b321fb6442db4ae9a49
Vitor Figueiredo <vitorfigueiredodev@outlook.com>
2025-01-10 15:05:54 -0300

    feat(projects) projects section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Header } from './components/Header'
 import { MainText } from './components/MainText'
 import { Skills } from './components/Skills'
 import { Footer } from './components/Footer'
+import { Projects } from './components/Projects'
 
 function App() {
 
@@ -9,6 +10,8 @@ function App() {
     <div className='p-5 pb-1 md:px-20 h-screen'>
       <Header />
       <MainText />
+      <div className='h-80'></div>
+      <Projects />
       <div className='h-80'></div>
       <Skills />
       <div className='h-24'></div>
---
+++ src/components/Projects.jsx
@@ -0,0 +1,10 @@
+
+export const Projects = () => {
+
+  return(
+    <section>
+      <h1 className='text-3xl font-black mt-24'>Projects</h1>
+      <p className='text-center w-full mt-96 text-4xl font-black'>Coming soon...</p>
+    </section>
+  )
+}
